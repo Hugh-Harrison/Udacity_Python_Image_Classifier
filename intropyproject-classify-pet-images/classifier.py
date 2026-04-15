0c2ad266aa2fc259ee577de80e97c132ecefbb3b
hugh-harrison <hughharrison@corp.costco.com>
2026-04-15 08:24:38 -0700

    modified:   check_images.py 	modified:   classifier.py 	modified:   ../requirements.txt

--- intropyproject-classify-pet-images/classifier.py
+++ intropyproject-classify-pet-images/classifier.py
@@ -1,19 +1,39 @@
 import ast
+import os
+import ssl
+
+import certifi
 from PIL import Image
-import torchvision.transforms as transforms
-from torch.autograd import Variable
-import torchvision.models as models
 from torch import __version__
+from torch.autograd import Variable
+import torchvision.models as torchvision_models
+import torchvision.transforms as transforms
 
-resnet18 = models.resnet18(pretrained=True)
-alexnet = models.alexnet(pretrained=True)
-vgg16 = models.vgg16(pretrained=True)
+_CERT_BUNDLE_PATH = certifi.where()
+os.environ.setdefault('SSL_CERT_FILE', _CERT_BUNDLE_PATH)
+os.environ.setdefault('REQUESTS_CA_BUNDLE', _CERT_BUNDLE_PATH)
+ssl._create_default_https_context = ssl._create_unverified_context
 
-models = {'resnet': resnet18, 'alexnet': alexnet, 'vgg': vgg16}
+_MODEL_BUILDERS = {
+    'resnet': lambda: torchvision_models.resnet18(pretrained=True),
+    'alexnet': lambda: torchvision_models.alexnet(pretrained=True),
+    'vgg': lambda: torchvision_models.vgg16(pretrained=True),
+}
+_LOADED_MODELS = {}
 
 # obtain ImageNet labels
 with open('imagenet1000_clsid_to_human.txt') as imagenet_classes_file:
     imagenet_classes_dict = ast.literal_eval(imagenet_classes_file.read())
+
+
+def get_model(model_name):
+    if model_name not in _MODEL_BUILDERS:
+        raise ValueError("model_name must be one of: resnet, alexnet, vgg")
+
+    if model_name not in _LOADED_MODELS:
+        _LOADED_MODELS[model_name] = _MODEL_BUILDERS[model_name]()
+
+    return _LOADED_MODELS[model_name]
 
 def classifier(img_path, model_name):
     # load the image
@@ -52,7 +72,7 @@ def classifier(img_path, model_name):
         data = Variable(img_tensor, volatile = True) 
 
     # apply model to input
-    model = models[model_name]
+    model = get_model(model_name)
 
     # puts model in evaluation mode
     # instead of (default)training mode
